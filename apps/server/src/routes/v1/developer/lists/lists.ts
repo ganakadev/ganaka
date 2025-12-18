0ad121f3b865af375fc4e71e4fe02a481bd8373d
shimorojune <rohitrajnovember@gmail.com>
2025-12-18 09:52:15 +0530

    Update HTTP headers in lists and scrape-shortlist modules for improved request handling
    - Updated User-Agent to the latest Chrome version for better compatibility.
    - Added additional headers including Accept, Accept-Language, Referer, and others to enhance request specificity and compliance with modern web standards.

--- apps/server/src/routes/v1/developer/lists/lists.ts
+++ apps/server/src/routes/v1/developer/lists/lists.ts
@@ -28,7 +28,17 @@ const listsRoutes: FastifyPluginAsync = async (fastify) => {
       const response = await axios.get(url, {
         headers: {
           "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+          Accept:
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+          "Accept-Language": "en-US,en;q=0.9",
+          Referer: "https://groww.in/",
+          "Accept-Encoding": "gzip, deflate, br",
+          "Sec-Fetch-Dest": "document",
+          "Sec-Fetch-Mode": "navigate",
+          "Sec-Fetch-Site": "none",
+          "Sec-Fetch-User": "?1",
+          "Upgrade-Insecure-Requests": "1",
         },
       });
 
